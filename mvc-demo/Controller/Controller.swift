48af01eb637876b03438fbd7ad5ff0d426d9c534
Mariah Baysic <mjpb123@gmail.com>
2020-03-30 14:06:01 +0800

    connecting model, view, and controller

--- mvc-demo/Controller/Controller.swift
+++ mvc-demo/Controller/Controller.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class Controller: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var colorLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appleProduct = AppleProduct(name: "iPhone 11", color: "Red", price: 999.99)
+        
+        nameLbl.text = "Model: \(appleProduct.name)"
+        colorLbl.text = "Color: \(appleProduct.color)"
+        priceLbl.text = "Price: $\(appleProduct.price)"
+        
     }
 
 
